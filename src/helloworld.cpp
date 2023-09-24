7a30ff78e7d44281abef8a10ef36e08df1856e00
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-09-24 20:15:46 -0300

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+string hello = "pedro";
+string name = "";
 
-std::cout << "Hello World"
+int persist;
+
+int main(int argc, char** argv[])
+{
+    cout << "Hello World" << endl; 
+
+    cout << "What is your name?" << endl;
+
+    cin >> name;
+
+    cout << "Your name is " + name;
+
+    cin >> persist;
+
+    return 0;
+}
