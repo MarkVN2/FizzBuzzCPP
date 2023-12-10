1fc741546af461758fd4286b0f38f20f49597872
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-12-10 19:12:13 -0300

    [QoL]: moved fizzbuzz into own function

--- src/fizzbuzz.cpp
+++ src/fizzbuzz.cpp
@@ -3,39 +3,46 @@
 #include <vector>  
 
 
+void fizzBuzz(int num, std::vector<int> nums, std::vector<std::string> text)
+    {
+    for (int i =1 ; i  <= num ; i++)
+        {
+            std::string output;
+            for (std::size_t j=0 ; j < text.size(); j++)
+            {
+                if (i % nums[j] ==0)
+                {
+                    output += text[j];
+                }
+            }
+                
+            if(output.empty())
+            {
+            std::cout << i << "\n";    
+            }
+            else
+            {
+                std::cout << output << "\n";
+            }
+    
+        }
+    };
+
 int main(int argc, char**)
 {
+    void fizzBuzz(int num, std::vector<int> nums, std::vector<std::string> text);  
+   
     int num;
-    
-    std::cout << "Please write a number for the limit of the FizzBuzz" << "\n";
+
+    printf("Please write a number for the limit of the FizzBuzz\n");
     
     std::cin >> num;
 
-    std::cout << "\n";
+    printf("\n");
+    
     
     std::vector<int> nums = {3,5};
     std::vector<std::string> text = {"Fizz","Buzz"};
-    
-    for (int i =1 ; i  <= num ; i++)
-    {
-        std::string output;
-        for (int j=0 ; j < text.size(); j++)
-        {
-            if (i % nums[j] ==0)
-            {
-                output += text[j];
-            }
-        }
-            
-        if(output.empty())
-        {
-           std::cout << i << "\n";    
-        }
-        else
-        {
-            std::cout << output << "\n";
-        }
-  
-    }
+    fizzBuzz(num, nums, text);
     return 0;
 }
